91aff971e5651983085392ec3370cf685344c27d
Rodrigo Verdugo <raverdugo@falabella.cl>
2021-10-13 14:32:17 -0300

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,46 @@
+const { response, request} = require("express");
+
+const usuariosGet = (req = request, res = response) => {
+    const {q,nombre= 'No mane',apikey, page= 1, limit} = req.query;
+  res.json({
+    msg: "get API - Controlador",
+    q,nombre,apikey, page, limit
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+    const {id} = req.params;
+  res.status(200).json({
+    msg: "put API - Controlador",
+    id
+  });
+};
+
+const usuariosPost = (req, res = response) => {
+  const {nombre,edad} = req.body;
+
+  res.status(201).json({
+    msg: "post API - Controlador",
+    nombre,edad
+  });
+};
+
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: "delete API - Controlador",
+  });
+};
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    msg: "patch API - Controlador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+};
